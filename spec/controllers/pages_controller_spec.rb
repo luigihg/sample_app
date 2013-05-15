a4a9297133fd477ff7d634bc3055a344ef998937
Luigi Hernandez Garcia <luigihg@gmail.com>
2013-05-15 13:55:38 -0500

    Added site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,6 +15,11 @@ render_views
           :content => "Ruby on Rails Tutorial Sample App | Home")
       end
 
+    it "should have a non-blank body" do
+       get 'home'
+     response.body.should_not =~ /<body>\s*<\/body>/
+      end
+
   end
 
   describe "GET 'contact'" do
